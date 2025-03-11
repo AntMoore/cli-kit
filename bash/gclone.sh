b335c02532d4ce40942fb8a6dc0d5ac899e2bfdc
Antonio Moore <Antonio.Moore@mso.finova.tech>
2025-03-11 14:40:43 +0000

    Update a couple o scripts

--- bash/gclone.sh
+++ bash/gclone.sh
@@ -7,6 +7,15 @@ gclone() {
         return 1
     fi
 
+    # Navigate to the projects folder before trying to clone
+    echo "Navigating to projects folder"
+    echo
+    gotoprojects
+
+    local BLUE="\e[1;34m"
+    local YELLOW="\e[1;33m"
+    local RESET="\e[0m"
+
     # Handle --help or -h flag
     if [[ "$1" == "--help" || "$1" == "-h" ]]; then
         echo "Usage: gclone <repository-url>"
@@ -17,6 +26,19 @@ gclone() {
         return 0
     fi
 
-    # Clone the repository and cd into it
-    git clone "$1" && cd "$(basename "$1" .git)"
+    local repo_name="$(basename "$1" .git)"
+
+    if [[ -d "$repo_name" ]]; then
+        echo -e ${YELLOW}"Repo already exists, navigating to folder"${RESET}
+        echo
+
+        cd "$(basename "$1" .git)"
+    else
+        echo -e ${BLUE}"Cloning the repo down, and navigating to folder"${RESET}
+        echo
+
+        # Clone the repository and cd into it
+        git clone "$1" && cd "$(basename "$1" .git)"
+    fi
+
 }
---
+++ bash/playground.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+goplay() {
+    echo "nobody playing today"
+}
